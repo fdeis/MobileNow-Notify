f53aa7d5f4b78d1e256159e7306f40fdc55356e5
Federico Deis <fdeis@mac.com>
2022-06-20 20:31:21 -0500

    Initial Commit

---
+++ MobileNow Notifier/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MobileNow Notifier
+//
+//  Created by Federico Deis on 6/20/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
